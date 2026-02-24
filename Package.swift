8ca38936f83160817b4ccb500cbc945a5a13532e
Sourav Kumar Singh <sourav.singh.ext@heromotocorp.com>
2026-02-24 12:16:53 +0530

    Update to v1.0.1 with new xcframework checksum

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FlashNotifications",
-            url: "https://github.com/Souravkumarsingh/FlashNotificationsSource/releases/download/v1.0.0/FlashNotifications.xcframework.zip",
-            checksum: "879f89b2717ffc841911ce9744474334674336fd911b35ac28a1a85b1398dd0a"
+            url: "https://github.com/Souravkumarsingh/FlashNotificationsSource/releases/download/v1.0.1/FlashNotifications.xcframework.zip",
+            checksum: "12c82a69835a18610abd705eda265f6caa61982aa21b6ee75592b36a287abb56"
         )
     ]
 )
