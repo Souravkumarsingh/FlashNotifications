b7db1085bf6a28c3a9346ecc4a97f2b018eafcfb
Sourav Kumar Singh <sourav.singh.ext@heromotocorp.com>
2026-04-17 00:45:20 +0530

    - checksum updated

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "FlashNotifications",
             url: "https://github.com/Souravkumarsingh/FlashNotifications/releases/download/v1.0.1/FlashNotifications.xcframework.zip",
-            checksum: "dfe8498a03d3a26568bd71054f5f48577f10e320df2d6fd4fde129e8438b0649"
+            checksum: "709b2e4e5430856d549eb0c6fa2d7db7c95223d5c7d1a971ee04e0635edc780a"
         )
     ]
 )
