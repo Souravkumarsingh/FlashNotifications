ee4258257dbaef787b253911ddd845cd09ddca4f
Flash Notifications <test@example.com>
2026-04-20 22:37:38 +0530

    Fix: Point Package.swift to 1.0.0 release

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FlashNotifications",
-            url: "https://github.com/Souravkumarsingh/FlashNotifications/releases/download/1.0.1/FlashNotifications.xcframework.zip",
+            url: "https://github.com/Souravkumarsingh/FlashNotifications/releases/download/1.0.0/FlashNotifications.xcframework.zip",
             checksum: "4ff5a2804c35f0f12b3f77ef5e31456c41428117731cca7c07f8f29a3bd6f04a"
         )
     ]
